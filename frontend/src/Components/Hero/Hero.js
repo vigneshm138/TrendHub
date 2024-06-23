1daabc4766a7e02f2b8ff1fd13f9f3d20e8386e1
vigneshM138 <vigneshmb2001@gmail.com>
2024-06-24 01:55:16 +0530

    hero component finish

---
+++ frontend/src/Components/Hero/Hero.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Hero.css";
+import { NavLink } from "react-router-dom";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero-banner">
+        <div className="hero-banner-para">
+          <h4>new arrivals only</h4>
+          <p>🔥new collections for everyone🔥</p>
+          <NavLink to={"/mens"}>Check out</NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
